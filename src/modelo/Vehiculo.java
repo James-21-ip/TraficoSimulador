939516ca3755fe6276bd4611aaf32fd477a48258
Jerik <jerikxxxd123@gmail.com>
2026-07-18 23:41:03 -0500

    Agrego clase Vehiculo con movimiento base

--- src/modelo/Vehiculo.java
+++ src/modelo/Vehiculo.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author PERCI
- */
-public class Vehiculo {
-    
+public abstract class Vehiculo {
+
+    protected double x;
+    protected double y;
+    protected double velocidad;
+    protected double velocidadMaxima;
+    protected double aceleracion;
+    protected double ancho;
+    protected double largo;
+    protected Carril carril;
+
+    public Vehiculo(double x, double y, Carril carril) {
+        this.x = x;
+        this.y = y;
+        this.carril = carril;
+        this.velocidad = 0;
+    }
+
+    protected abstract void definirCaracteristicas();
+
+    public void mover(Vehiculo vehiculoAdelante, double deltaTime) {
+        double distanciaSegura = velocidad * 1.5;
+        double distanciaLibre = (vehiculoAdelante != null)
+                ? distanciaHacia(vehiculoAdelante)
+                : Double.MAX_VALUE;
+
+        // si hay alguien muy cerca adelante, frenamos
+        if (distanciaLibre < distanciaSegura) {
+            velocidad = Math.max(0, velocidad - aceleracion * 2 * deltaTime);
+        } else if (velocidad < velocidadMaxima) {
+            velocidad = Math.min(velocidadMaxima, velocidad + aceleracion * deltaTime);
+        }
+
+        x += velocidad * deltaTime; // TODO: ajustar cuando tengamos la direccion real del carril
+    }
+
+    protected double distanciaHacia(Vehiculo otro) {
+        return Math.hypot(otro.x - this.x, otro.y - this.y);
+    }
+
+    public java.awt.geom.Rectangle2D getHitbox() {
+        return new java.awt.geom.Rectangle2D.Double(x - largo / 2, y - ancho / 2, largo, ancho);
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getVelocidad() { return velocidad; }
+    public Carril getCarril() { return carril; }
 }
